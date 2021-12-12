b09827e1e2770915cf8325484155e5fd4f180413
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-13 00:58:33 +0530

    slice, splice

--- 03 Intermediate/09 filterArray.js
+++ 03 Intermediate/09 filterArray.js
@@ -1,5 +1,5 @@
 var testArray = [2, 3, 5, 6, 7, 1, 4, 9];
-console.log(testArray.fill("h", 0));
+// console.log(testArray.fill("h", 0));
 // output ->
 // [
 // 'h', 'h', 'h', 'h',
@@ -7,10 +7,20 @@ console.log(testArray.fill("h", 0));
 // ]
 
 var testArray = [2, 3, 5, 6, 7, 1, 4, 9];
-console.log(testArray.fill("empty"));   // 2 is inclusive, 5 is exclusive.
+// console.log(testArray.fill("empty"));   // 2 is inclusive, 5 is exclusive.
 
 const myNumber = [24, 65, 56, 33, 66, 74, 86];
-
 var result = myNumber.filter((num) => num > 56);
+// console.log(result);
 
-console.log(result);
+
+// Slice
+var users = ["Sohail", "Manjit", "Ankur", "Tushar", "Siddheshya", "Panda Bear", "Rabby Catty"];
+console.log(users.slice(2, 5));  // the start is inclusive and end is exclusive.
+console.log(users.slice(4));  // removes all the elements before 4
+
+
+// Splice
+users.splice(2, 3, "Hi", "Bye") // 1 -> start position and 2 -> count.
+// start from 1 and till count exchange the element with the given argument.
+console.log(users);
