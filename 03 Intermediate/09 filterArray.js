c8328c0a2ab398df0476e238d22c01eaa2070501
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-13 00:40:37 +0530

    filter,fill

--- 03 Intermediate/09 filterArray.js
+++ 03 Intermediate/09 filterArray.js
@@ -7,4 +7,10 @@ console.log(testArray.fill("h", 0));
 // ]
 
 var testArray = [2, 3, 5, 6, 7, 1, 4, 9];
-console.log(testArray.fill("h", 2));
+console.log(testArray.fill("empty"));   // 2 is inclusive, 5 is exclusive.
+
+const myNumber = [24, 65, 56, 33, 66, 74, 86];
+
+var result = myNumber.filter((num) => num > 56);
+
+console.log(result);
