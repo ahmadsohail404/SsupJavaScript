d6003439b2808025451c673cbcd64551f41e1d3c
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-14 16:19:22 +0530

    loops 2

--- 03 Intermediate/12 loops.js
+++ 03 Intermediate/12 loops.js
@@ -1,3 +1,4 @@
+export { };
 // for (let i = 0; i <= 10; i++) {
 //     console.log(i);
 // }
---
+++ 03 Intermediate/13 loopsPartTwo.js
@@ -0,0 +1,20 @@
+const myStates = [
+    "Bihar",
+    "Karnataka",
+    "Delhi",
+    1947,
+    "Maharashtra",
+    "Haryana",
+    "Tamil Nadu"
+];
+
+// var i = 0;
+// for (; ;) {
+//     if (i > 3) break;
+//     console.log(i);
+//     i++;
+// }
+
+myStates.forEach((x) => (console.log(x)));  // 🔥lit
+myStates.forEach(x => console.log(x)); // without parenthesis
+
