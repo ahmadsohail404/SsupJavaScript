9331d5d7e359107b895190812ca14358afea8fd2
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-17 03:46:04 +0530

    color console

---
+++ Color Console/script.js
@@ -0,0 +1,14 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+
+const center = document.querySelector(".center");
+
+// console.log(window.getComputedStyle(red).backgroundColor);
+
+const getBGColor = (selectedColor) => {
+    return (window.getComputedStyle(selectedColor).backgroundColor);
+}
+console.log(getBGColor(orange));
